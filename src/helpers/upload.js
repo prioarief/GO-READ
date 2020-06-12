744098b63a578708a1884e341bc37919d8314d8a
prioarief <prioariefgunawangunawan@gmail.com>
2020-06-12 13:48:45 +0700

    public directory

--- src/helpers/upload.js
+++ src/helpers/upload.js
@@ -1,7 +1,7 @@
 const multer = require("multer")
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, './src/images/')
+        cb(null, './public/images/')
     },
     filename: (req, file, cb) => {
         const image = file.originalname
