5f7c0447b1a9a54078179c8e16a46a621117d9ab
prioarief <prioariefgunawangunawan@gmail.com>
2020-08-16 23:05:35 +0700

    fixing bug

--- src/models/Auth.js
+++ src/models/Auth.js
@@ -1,106 +1,132 @@
-const connection = require("../config/database");
+const connection = require('../config/database');
 module.exports = {
-    getAll : (show, page) => {
-        return new Promise((resolve, reject) => {
-            connection.query(`SELECT * FROM authors`, (error, result) => {
-                if(error){
-                    reject(error)
-                }
-                resolve(result)
-            })
-        })
-    },
+	getAll: (show, page) => {
+		return new Promise((resolve, reject) => {
+			connection.query(`SELECT * FROM authors`, (error, result) => {
+				if (error) {
+					reject(error);
+				}
+				resolve(result);
+			});
+		});
+	},
 
-    login :  (email) => {
-            return new Promise((resolve, reject) => {
-                connection.query('SELECT u.id, u.email, u.password, u.name, u.is_active, u.created_at, r.role FROM users u JOIN roles r ON r.id = u.role WHERE u.email = ? ', email, (error, result) => {
-                    if(error){
-                        reject(error)
-                    }
-                    resolve(result)
-                })
-            }) 
-    },
+	login: (email) => {
+		return new Promise((resolve, reject) => {
+			connection.query(
+				'SELECT u.id, u.email, u.password, u.name, u.is_active, u.created_at, r.role FROM users u JOIN roles r ON r.id = u.role WHERE u.email = ? ',
+				email,
+				(error, result) => {
+					if (error) {
+						reject(error);
+					}
+					resolve(result);
+				}
+			);
+		});
+	},
 
-    register : (setData) => {
-        return new Promise((resolve, reject) => {
-            connection.query('INSERT INTO users SET ?', setData, (error, result) => {
-                if(error){
-                    reject(error)
-                }
-                
-                const newData ={
-                    id : result.insertId,
-                    ...setData
-                }
-                resolve(newData)
-            })
-        })
-    },
+	register: (setData) => {
+		return new Promise((resolve, reject) => {
+			connection.query('INSERT INTO users SET ?', setData, (error, result) => {
+				if (error) {
+					reject(error);
+				}
 
-    editUsers: (setData, id) => {
-        return new Promise((resolve, reject) => {
-            connection.query('UPDATE users SET ? WHERE id=?', [setData, id], (error, result) => {
-                if(error){
-                    reject(error)
-                }
-                resolve(result)
-            })
-        })
-    },
+				if (result) {
+					const newData = {
+						id: result.insertId,
+						...setData,
+					};
+					resolve(newData);
+				}
+			});
+		});
+	},
 
-    deleteUser : (id) => {
-        return new Promise((resolve, reject) => {
-            connection.query('DELETE FROM users WHERE id=?', id, (error, result) => {
-                if(error){
-                    reject(error)
-                }
-                resolve(result)
-            })
-        })
-    },
-    
-    detailUser : (id) => {
-        return new Promise((resolve, reject) => {
-            connection.query('SELECT u.*, r.role FROM users u JOIN roles r ON r.id = u.role WHERE u.id = ? ', id, (error, result) => {
-                if(error){
-                    reject(error)
-                }
-                resolve(result)
-            })
-        })
-    },
-    
-    insertCode : (data) => {
-        return new Promise((resolve, reject) => {
-            connection.query('INSERT INTO verification SET ? ', data, (error, result) => {
-                if(error){
-                    reject(error)
-                }
-                resolve(result)
-            })
-        })
-    },
-    
-    getCode : (email) => {
-        return new Promise((resolve, reject) => {
-            connection.query('SELECT * FROM verification WHERE email = ? ', email, (error, result) => {
-                if(error){
-                    reject(error)
-                }
-                resolve(result)
-            })
-        })
-    },
-    
-    deleteCode : (email) => {
-        return new Promise((resolve, reject) => {
-            connection.query('DELETE FROM verification WHERE email = ? ', email, (error, result) => {
-                if(error){
-                    reject(error)
-                }
-                resolve(result)
-            })
-        })
-    },
-}
+	editUsers: (setData, id) => {
+		return new Promise((resolve, reject) => {
+			connection.query(
+				'UPDATE users SET ? WHERE id=?',
+				[setData, id],
+				(error, result) => {
+					if (error) {
+						reject(error);
+					}
+					resolve(result);
+				}
+			);
+		});
+	},
+
+	deleteUser: (id) => {
+		return new Promise((resolve, reject) => {
+			connection.query('DELETE FROM users WHERE id=?', id, (error, result) => {
+				if (error) {
+					reject(error);
+				}
+				resolve(result);
+			});
+		});
+	},
+
+	detailUser: (id) => {
+		return new Promise((resolve, reject) => {
+			connection.query(
+				'SELECT u.*, r.role FROM users u JOIN roles r ON r.id = u.role WHERE u.id = ? ',
+				id,
+				(error, result) => {
+					if (error) {
+						reject(error);
+					}
+					resolve(result);
+				}
+			);
+		});
+	},
+
+	insertCode: (data) => {
+		return new Promise((resolve, reject) => {
+			connection.query(
+				'INSERT INTO verification SET ? ',
+				data,
+				(error, result) => {
+					if (error) {
+						reject(error);
+					}
+					resolve(result);
+				}
+			);
+		});
+	},
+
+	getCode: (email) => {
+		return new Promise((resolve, reject) => {
+			connection.query(
+				'SELECT * FROM verification WHERE email = ? ',
+				email,
+				(error, result) => {
+					if (error) {
+						reject(error);
+					}
+					resolve(result);
+				}
+			);
+		});
+	},
+
+	deleteCode: (email) => {
+		return new Promise((resolve, reject) => {
+			connection.query(
+				'DELETE FROM verification WHERE email = ? ',
+				email,
+				(error, result) => {
+					if (error) {
+						reject(error);
+					}
+					resolve(result);
+				}
+			);
+		});
+	},
+};
